f7aacbc1ddf15c8bd7628145f30a01f9aaf28119
mi6kata <73338812+mimars2005@users.noreply.github.com>
2023-02-20 00:31:08 +0200

    dsds

--- can_software/lib/main.dart
+++ can_software/lib/main.dart
@@ -25,71 +25,91 @@ class _MyAppState extends State<MyApp> {
               child: Center(
                 child: Row(
                   children: [
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SfRadialGauge(
-                            enableLoadingAnimation: true,
-                            axes: <RadialAxis>[
-                              RadialAxis(
-                                interval: 45,
-                                minimum: 0,
-                                maximum: 180,
-                                ranges: <GaugeRange>[
-                                  GaugeRange(
-                                      startValue: 0,
-                                      endValue: 180,
-                                      gradient: SweepGradient(colors: [
-                                        Colors.green,
-                                        Colors.orange,
-                                        Colors.red
-                                      ], stops: const <double>[
-                                        0.0,
-                                        0.5,
-                                        1
-                                      ])),
+                    Container(
+                      child: Expanded(
+                        flex: 2,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 700,
+                              child: SfRadialGauge(
+                                enableLoadingAnimation: true,
+                                axes: <RadialAxis>[
+                                  RadialAxis(
+                                    axisLabelStyle: GaugeTextStyle(
+                                        color: Colors.black, fontSize: 15),
+                                    interval: 45,
+                                    minimum: 0,
+                                    maximum: 180,
+                                    ranges: <GaugeRange>[
+                                      GaugeRange(
+                                          startValue: 0,
+                                          endValue: 180,
+                                          gradient: SweepGradient(colors: [
+                                            Colors.green,
+                                            Colors.orange,
+                                            Colors.red
+                                          ], stops: const <double>[
+                                            0.0,
+                                            0.5,
+                                            1
+                                          ])),
+                                    ],
+                                    pointers: <GaugePointer>[
+                                      NeedlePointer(
+                                          value: gaugeValue,
+                                          needleLength: 0.8,
+                                          enableAnimation: true,
+                                          animationType: AnimationType.ease,
+                                          knobStyle: KnobStyle(
+                                            knobRadius: 0.1,
+                                            borderColor: Colors.blue,
+                                            borderWidth: 0.009,
+                                          )),
+                                    ],
+                                  )
                                 ],
-                                pointers: <GaugePointer>[
-                                  NeedlePointer(
-                                      value: gaugeValue,
-                                      needleLength: 0.8,
-                                      enableAnimation: true,
-                                      animationType: AnimationType.ease,
-                                      knobStyle: KnobStyle(
-                                        knobRadius: 0.1,
-                                        borderColor: Colors.blue,
-                                        borderWidth: 0.009,
-                                      )),
-                                ],
-                              )
-                            ],
-                          ),
-                        ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     Expanded(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          ElevatedButton(
-                              onPressed: () => {
-                                    setState(() {
-                                      if (gaugeValue == 0) {
-                                        gaugeValue = 45;
-                                      } else if (gaugeValue == 45) {
-                                        gaugeValue = 0;
-                                      }
-                                    }),
-                                  },
-                              child: Text("+")),
                           Container(
-                            height: 100,
+                            height: 150,
+                            width: 350,
+                            child: ElevatedButton(
+                                onPressed: () => {
+                                      setState(() {
+                                        if (gaugeValue == 0) {
+                                          gaugeValue = 45;
+                                        } else if (gaugeValue == 45) {
+                                          gaugeValue = 0;
+                                        }
+                                      }),
+                                    },
+                                child: Text(
+                                  "NEXT",
+                                  style: TextStyle(fontSize: 25),
+                                )),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 40),
+                            height: 150,
+                            width: 350,
                             child: ElevatedButton(
                                 onPressed: () {
                                   setState(() {});
                                 },
-                                child: Text("-")),
+                                child: Text(
+                                  "BACK",
+                                  style: TextStyle(fontSize: 25),
+                                )),
                           )
                         ],
                       ),
